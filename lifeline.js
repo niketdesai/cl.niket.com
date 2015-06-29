922cbc299479699989da94b5530dc34fd648297e
Niket Desai <iamniket@gmail.com>
2015-06-29 20:10:04 +0530

    added updates for 2015

--- lifeline.js
+++ lifeline.js
@@ -2,16 +2,16 @@ var data = [
   {
     "release": "com.desai.niket 3.1",
     "year": 2015,
-    "img": "img/26.jpg",
+    "img": "img/",
     "changes" : [
-      "I move to Bangalore, India, to join Flipkart as Chief of Staff.",
-      "Countries visited: Denmark, England, Hong Kong, India, Sweden, Taipei",
+      "I move to Bangalore, India, to join Flipkart as Chief of Staff for my old Google & Motorola VP, Punit Soni.",
+      "Countries visited: Denmark, Egypt, England, Hong Kong, India, Malaysia, Mexico, Sweden, Taipei, United Arab Emirates",
     ],
   },
   {
     "release": "com.desai.niket 3.1",
     "year": 2014,
-    "img": "img/26.jpg",
+    "img": "img/",
     "changes" : [
       "Motorola is sold to electronics manufacturer Lenovo. I return to Google in search of a new role.",
       "Extensively traveled to Motorola's Chicago-based HQ, and become acquainted with the great windy city.",
@@ -22,167 +22,240 @@ var data = [
   {
     "release": "com.desai.niket 3.0",
     "year": 2013,
+    "img": "img/",
     "changes" : [
-      "I continue to work at Google, but have joined Motorola as a Product Manager to reinvigorate the once venerable company under Punit Soni",
-      "I saw the third launch of Space X's Dragon at Cape Canaveral with Nat Welch.",
-      "Trained at a ",
-      "Underwent BEP Chemotherapy to stop Cancer from spreading throughout my body. It was, and will be, the worst experience in my entire life.",
-      "Underwent Septoplasty surgery to help me sleep and rest from residual issues related to chemotherapy.",
-      "Countries visited: India, Korea, Malaysia, Singapore, Malaysia",
+      "I continue to work at Google joining their newly acquired Motorola Division to help reinvigorate the once venerable company.",
+      "Attend the third launch of Space X's Dragon at Cape Canaveral with Nat Welch - a personal bucket list item.",
+      "Undergo chemotherapy to stop cancer from spreading throughout my body. It was, and will be, the worst experience of entire life.",
+      "Punchd is formally shut down, accomplishing far less than I had hoped.",
+      "Countries visited: India, Korea, Malaysia, Singapore, ",
     ],
   },
   {
     "release": "com.desai.niket 2.",
     "year": 2012,
-    "img": "img/24.jpg",
+    "img": "img/",
     "changes" : [
-      "I was diagnosed with Cancer at PAMF in Mountain View with my Dad and Matt Pies present.",
-      "Underwent two GA surgeries: Radical Orchiectemy, Retroperitoneal Lymph Node Dissection (RPLND).",
-      "Serve as groomsman in Xander's wedding in SLO. Attend 4 weddings.",
-      "I dated 1 woman.",
-      "Countries visited: England, Finland, Hong Kong, India Mexico, Singapore" ,
-      
+      "Diagnosed with Cancer at PAMF in Mountain View with my Dad and Matt Pies present.",
+      "Complete two GA surgeries: Radical Orchiectemy, Retroperitoneal Lymph Node Dissection (RPLND).",
+      "Serve as groomsman in Xander and Maleesa's wedding in SLO. Attend four other weddings.",
+      "Dated 1 woman.",
+      "Countries visited: England, Finland, Hong Kong, India, Mexico, Singapore",
     ],
   },
   {
     "release": "com.desai.niket 2.3",
-    "img": "",
+    "img": "img/",
     "year": 2011,
     "changes" : [
-      "Punchd was acquired by Google, into the Mobile Apps Lab, under Marissa Mayer's Geo Unit",
-      "Punchd ",
-      "Countries visited: France, Hong Kong, India, Macau" ,
+      "Punchd is acquired by Google, into the Mobile Apps Lab, under Marissa Mayer's GeoLocal unit.",
+      "Countries visited: France, Hong Kong, India, Macau",
     ],
   },
   {
     "release" : "com.desai.niket 2.2",
     "year": 2010,
-    "img" : "",
+    "img": "img/",
     "changes" : [
+      "Founded Punchd with Reed Morse and Xander Pollock with the intention of helping small businesses navigate through a digital world.",
+      "Moved to San Francisco with Michael Mizono.",
       "Countries visited: Germany",
     ]
   },
   {
     "release" : "com.desai.niket 2.1",
-    "img": "img/21.jpg",
+    "img": "img/",
     "year": 2009,
     "changes" : [
+      "I meet Reed Morse, an enterprising computer scientist and tinkerer on a double-date in San Francisco.",
+      "Graduate from Berkeley with a degree in Industrial Engineering and Operations Research along with some of the best friends I'll have.",
       "Countries visited: ",
     ]
   },
   {
     "release" : "com.desai.niket 2.0",
     "year": 2008,
+    "img": "img/",
     "changes" : [
       "Countries visited: ",
     ]
   },
   {
-    "release": "com.desai.niket 1.9",
+    "release": "com.desai.niket 1.8",
     "year": 2007,
+    "img": "img/",
     "changes": [
-      "Countries visited: ",
-    ]
-  },
-  {
-    "release" : "com.desai.niket 1.8",
-    "year": 2006,
-    "changes" : [
+      "Transferred to Berkeley and joined the Delta Chi fraternity.",
       "Countries visited: ",
     ]
   },
   {
     "release" : "com.desai.niket 1.7",
-    "year": 2005,
+    "year": 2006,
+    "img": "img/",
     "changes" : [
-      "Achieved Eagle Scout rank with the BSA, capping a 10 year journey.",
-      "Countries visited: France, Italy, Spain",
+      "Grandmother passes away on April Fool's Day. Joke was on us.",
+      "Countries visited: ",
     ]
   },
   {
     "release" : "com.desai.niket 1.6",
-    "year": 2004,
+    "year": 2005,
+    "img": "img/",
     "changes" : [
+      "Start school at De Anza College aiming to learn a mix of Mathematics or Engineering. Become a senior Math Tutor.",
+      "Earn Eagle Scout rank with the BSA, capping a 10 year journey that began in Singapore.",
+      "Countries visited: France, Italy, Spain",
+    ]
+  },
+  {
+    "release" : "com.desai.niket 1.5",
+    "year": 2004,
+    "img": "img/",
+    "changes" : [
+      "Arrested for trespassing at my former Kindergarten School. Banned from being within 100 feet for three years.",
       "Countries visited: Singapore",
     ]
   },
   {
-    "release" : "com.desai.niket 1.5",
+    "release" : "com.desai.niket 1.4",
     "year": 2003,
+    "img": "img/",
     "changes" : [
+      "Return to the USA from Singapore entering Monta Vista High School as a Junior.",
       "Countries visited: India",
     ]
   },
   {
-    "release" : "com.desai.niket 1.5",
+    "release" : "com.desai.niket 1.3",
     "year": 2002,
+    "img": "img/",
     "changes" : [
+      "Dad gets laid off from our expat job, life becomes uncertain for the Desai family.",
       "Countries visited: China, India, Indonesia, Malaysia",
     ]
   },
   {
-    "release" : "com.desai.niket 1.5",
+    "release" : "com.desai.niket 1.2",
     "year": 2001,
+    "img": "img/",
     "changes" : [
       "Countries visited: Australia, India, Indonesia, Malaysia, New Zealand ",
     ]
   },
   {
-    "release" : "com.desai.niket 1.5",
+    "release" : "com.desai.niket 1.1",
     "year": 2000,
+    "img": "img/",
     "changes" : [
-      "Countries visited: Cambodia, Malaysia, Thailand, Vietnam",
+      "Attend the 2000 Olympics in Sydney, Australia. Coincidentally, we have the best Indian food we've ever had there.",
+      "Countries visited: Australia, Cambodia, Malaysia, Thailand, Vietnam",
     ]
   },
   {
     "release" : "com.desai.niket 1.0",
     "year": 1999,
+    "img": "img/",
     "changes" : [
-      "I moved to Singapore with family",
+      "I start 7th standard at the Singapore American School. Solidify my taste for engineering taking Civil Engineering with Professor Diebly.",
+      "Our family moves to Singapore as my Dad pursues a larger role with Chartered Semiconductor.",
       "Countries visited: India, Indonesia, Singapore",
     ]
   },
   {
-    "release" : "com.desai.niket 0.9",
+    "release" : "com.desai.niket 0.12",
     "year": 1998,
+    "img": "img/",
     "changes" : [
-      "Countries visited: England, France",
+      "Luged down Mt. Playtpus.",
+      "Attend World Cup in France. Watch French beat Italy on their way to becoming World Champions.",
+      "Left Regnart Elementary as a 5th standard to enter 6th standard at John F. Kennedy Middle School.",
+      "Countries visited: England, France, Switzerland",
     ]
   },
   {
-    "release" : "com.desai.niket 0.5",
-    "year": 1993,
+    "release" : "com.desai.niket 0.11",
+    "year": 1997,
+    "img": "img/",
     "changes" : [
-      "Moved into 1227 St. Matthew Way, Los Altos, CA.",
-      "Began Kindergarten at Montclaire Elementary School.",
+      "Florida southern most place in US",
+      "Cruise to Jamaica ",
+      "Countries visited: Mexico, Jaimaca ",
     ]
   },
   {
-    "release" : "com.desai.niket 0.4",
-    "year": 1992,
+    "release" : "com.desai.niket 0.10",
+    "year": 1996,
+    "img": "img/",
     "changes" : [
-      "Started pre-school and Ellen Thatcher. Met Aaron Shaw and Paul Morrill, became good friends.",
-    ]
-  },
-  {
-    "release" : "com.desai.niket 0.1",
-    "year": 1989,
-    "img": "img/1.jpg",
-    "changes" : [
+      "",
       "Countries visited: ",
     ]
   },
   {
-    "release" : "com.desai.niket 0.1",
+    "release" : "com.desai.niket 0.9",
+    "year": 1995,
+    "img": "img/",
+    "changes" : [
+      "Attend Summer Olympics in Atlanta, GA.",
+      "Countries visited: England, France",
+    ]
+  },
+  {
+    "release" : "com.desai.niket 0.8",
+    "year": 1994,
+    "img": "img/",
+    "changes" : [
+      "",
+      "Countries visited: England, France",
+    ]
+  },
+  {
+    "release" : "com.desai.niket 0.7",
+    "year": 1993,
+    "img": "img/",
+    "changes" : [
+      "Countries visited",
+    ]
+  },
+  {
+    "release" : "com.desai.niket 0.6",
+    "year": 1992,
+    "img": "img/",
+    "changes" : [
+      "Countries visited:",
+    ]
+  },
+  {
+    "release" : "com.desai.niket 0.5",
+    "year": 1991,
+    "img": "img/",
+    "changes" : [
+      "Moved to [READACTED], Cupertino CA.",
+      "New project: Natasha Desai Init on [REDACTED], 1991 11:31AM. Birthed at Good Samaritan in San Jose, CA.",
+      "Countries visited: ",
+    ]
+  },
+  {
+    "release" : "com.desai.niket 0.3",
+    "year": 1989,
+    "img": "img/",
+    "changes" : [
+      "Countries visited:",
+    ]
+  },
+  {
+    "release" : "com.desai.niket 0.2",
     "year": 1988,
-    "img": "img/1.jpg",
+    "img": "img/",
     "changes" : [
       "Countries visited: India",
     ]
   },
   {
-    "release" : "com.desai.niket 0.0",
+    "release" : "com.desai.niket 0.1",
     "year": 1987,
+    "img": "img/",
     "changes" : [
       "Project Init on [REDACTED], 1987 5:07AM. Initial maintainers Meena Desai and Naushik Desai. Birthed at Good Samaritan in San Jose, CA.",
     ]
